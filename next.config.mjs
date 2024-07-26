583ab23c4ce111e156c7721fc7360511f0905a24
Jayesh <jayeshpawar.1312@gmail.com>
2024-07-26 19:27:14 +0530

    changed path test

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 
-import path from "path";
 const nextConfig = {
   experimental: {
     outputFileTracingIncludes: {
-      "*": [path.join(process.cwd(), "data.db")],
+      "*": ["./data.db"],
     },
   },
 };
